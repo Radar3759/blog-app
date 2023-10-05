82e794cbc1eb71263bc75ad410fc37ecf30ea185
Radar3759 <chainmailgirl@gmail.com>
2023-10-05 13:24:36 -0700

    -remove insta section, alt blog title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <body class="bg-white font-family-karla">
 
         <!-- Top Bar Nav -->
-        <nav class="w-full py-4 bg-blue-800 shadow">
+        <!-- <nav class="w-full py-4 bg-blue-800 shadow">
             <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
 
                 <nav>
@@ -51,13 +51,13 @@
                 </div>
             </div>
 
-        </nav>
+        </nav> -->
 
         <!-- Text Header -->
         <header class="w-full container mx-auto">
             <div class="flex flex-col items-center py-12">
                 <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
-                    Minimal Blog
+                    Lorem Ipsum Blog
                 </a>
                 <p class="text-lg text-gray-600">
                     Lorem Ipsum Dolor Sit Amet
@@ -101,24 +101,6 @@
                     <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
                     <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
                         Get to know us
-                    </a>
-                </div>
-
-                <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-                    <p class="text-xl font-semibold pb-5">Instagram</p>
-                    <div class="grid grid-cols-3 gap-3">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=3">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=4">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=5">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=6">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=7">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=8">
-                        <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">
-                    </div>
-                    <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-                        <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
                     </a>
                 </div>
 
