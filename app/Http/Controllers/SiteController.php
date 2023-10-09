2348a623d2d980b8d1e6ecd30f62f142047efd43
Radar3759 <chainmailgirl@gmail.com>
2023-10-09 15:06:58 -0700

    -fix slug error

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\TextWidget;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SiteController extends Controller
 {
-    public function about()
+    public function about(): View
     {
-        
+        $widget = TextWidget::query()
+        ->where('active', '=', true)
+        ->first();
+
+        if(!$widget) {
+            throw new NotFoundHttpException();
+        }
+        return view('about', compact('widget'));
     }
 }
