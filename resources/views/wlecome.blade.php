4d47245db2f7873dba50ec5065012cdc3fb49d8c
Radar3759 <chainmailgirl@gmail.com>
2023-10-17 09:54:47 -0700

    -add welcome.blade install breeze

---
+++ resources/views/wlecome.blade.php
@@ -0,0 +1,23 @@
+  <?php
+    /** @var $posts Illuminate\Pagination\LengthAwarePaginator */
+  ?>
+  
+  <x-app-layout>
+  
+  <!-- Posts Section -->
+<section class="w-full md:w-2/3 flex flex-col items-center px-3">
+           
+        @foreach($posts as $post)
+            <x-post-item :post='$post'>
+
+            </x-post-item>
+
+        @endforeach 
+
+        {{$posts->onEachSide(1)->links()}}
+
+          
+        </section>
+            
+    <x-sidebar />
+  </x-app-layout>        
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,10 +14,18 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', [App\Http\Controllers\PostController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/about-us', [App\Http\Controllers\SiteController::class, 'about'])->name('about-us');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/category/{category:slug}', [App\Http\Controllers\PostController::class, 'byCategory'])->name('by-category');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/{post:slug}', [App\Http\Controllers\PostController::class, 'show'])->name('view');
+require __DIR__.'/auth.php';
