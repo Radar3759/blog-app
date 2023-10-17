6d9f8740e39701b84ee1a0048c983285d341d921
Radar3759 <chainmailgirl@gmail.com>
2023-10-17 10:58:35 -0700

    -revert app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,84 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{ $metaTitle ?: 'Pizza Ipsum Blog' }}</title>
+        <meta name="author" content="Pizza Ipsum Blog">
+        <meta name="description" content=>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Tailwind -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+            .font-family-karla {
+                font-family: karla;
+            }
+        </style>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- AlpineJS -->
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        <!-- Font Awesome -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="bg-gray-50 font-family-karla">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+
+            <!-- Text Header -->
+                <header class="w-full container mx-auto">
+                    <div class="flex flex-col items-center py-12">
+                <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="{{route('home')}}">
+                    Pizza Ipsum Blog
+                </a>
+                <p class="text-lg text-gray-600">
+                    {{ \App\Models\TextWidget::getTitle('header')}}
+</p>
                     </div>
                 </header>
-            @endif
+            
+        <!-- Topic Nav -->
+        <nav class="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
+            <div class="block sm:hidden">
+                <a
+                    href="#"
+                    class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
+                    @click="open = !open"
+                >
+                    Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
+                </a>
+            </div>
+            <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+                <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+                <a href="{{route('home')}}"
+                class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">
+                    Home
+                </a>    
+                @foreach($categories as $category) 
+                    <a href='{{route('by-category', $category)}}' class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">{{$category->title}}</a>
+                @endforeach
+                <a href='{{route('about-us')}}' class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">
+                    About Us
+                </a>    
+                </div>
+            </div>
+        </nav>
 
-            <!-- Page Content -->
-            <main>
+        <div class="container mx-auto flex flex-wrap py-6">
+
                 {{ $slot }}
-            </main>
+            
         </div>
+
+        <footer class="w-full border-t bg-white pb-12">
+          
+            <div class="w-full container mx-auto flex flex-col items-center">
+               
+                <div class="uppercase py-6">&copy; loremIpsumBlog.com</div>
+            </div>
+        </footer>
+
+   
     </body>
 </html>
+
